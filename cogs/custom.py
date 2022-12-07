4a54707a9f11088a73341bd774d43e291117fa69
SakuraKat <SakuraKat1F338@gmail.com>
2022-12-07 22:47:47 +0530

    housekeeping

--- cogs/custom.py
+++ cogs/custom.py
@@ -1,11 +1,3 @@
-""""
-Copyright © Krypton 2022 - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-This is a template to create your own discord bot in python.
-
-Version: 5.3
-"""
-
 import random
 
 from discord.ext import commands
